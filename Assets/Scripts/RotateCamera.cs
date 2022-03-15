5e66beea5d526ce0e8734621be392ffe072d78b8
adorelee <dfelton@aoiths.org>
2022-03-15 12:29:32 -0400

    Initial commit

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+       playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+         float horizontalInput = Input.GetAxis("Horizontal");
+         transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
